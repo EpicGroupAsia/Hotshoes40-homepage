cb2eeeec16bd8050dfb5aa635d621804b2d31646
EPIC <andyoba@gmail.com>
2026-07-01 11:23:31 +0800

    feat: redirect hotshoes.com.my to www.hotshoes.asia

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "hotshoes.com.my" }],
+        destination: "https://www.hotshoes.asia/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.hotshoes.com.my" }],
+        destination: "https://www.hotshoes.asia/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
